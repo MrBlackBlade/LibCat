004e92ed26fefb8c78dcaa08c68a9b54be25f81f
Islam Ahmed <islamahmedkader2004@gmail.com>
2023-12-15 16:03:28 +0200

    Modified Admin - Formatting - Method Encapsulation

--- src/libcat/Admin.java
+++ src/libcat/Admin.java
@@ -9,10 +9,12 @@ public class Admin extends User implements UserType {
     public Admin(int id, String name) {
         super(id, name);
     }
+
     @Override
     public String getType() {
         return "admin";
     }
+
     public String toString() {
         return String.format("Admin ID: %d, Admin Username: %s", getID(), getName());
     }
@@ -21,19 +23,19 @@ public class Admin extends User implements UserType {
         Library.customers.add(new Customer(id, name));
     }
 
-    public static void addBook(Book newBook) {
+    protected static void addBook(Book newBook) {
         Library.books.add(newBook);
     }
 
-    public static void updateBook(
-                                  int bookID,
-                                  String newTitle,
-                                  String newAuthor,
-                                  String newGenre,
-                                  String newYear,
-                                  double newPrice,
-                                  double newSalePercent,
-                                  boolean newAvailability
+    protected static void updateBook(
+            int bookID,
+            String newTitle,
+            String newAuthor,
+            String newGenre,
+            String newYear,
+            double newPrice,
+            double newSalePercent,
+            boolean newAvailability
     ) {
         for (Book book : Library.books) {
             if (book.getBookID() == bookID) {
@@ -49,17 +51,16 @@ public class Admin extends User implements UserType {
         }
     }
 
-    public static void deleteBook(int bookID) {
-            Iterator<Book> iterator = Library.books.iterator();
+    protected static void deleteBook(int bookID) {
+        Iterator<Book> iterator = Library.books.iterator();
 
-            while (iterator.hasNext()) {
-                Book book = iterator.next();
-                if (book.getBookID() == bookID) {
-                    iterator.remove();
-                    break;
-                }
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getBookID() == bookID) {
+                iterator.remove();
+                break;
             }
         }
-
+    }
 }
 
